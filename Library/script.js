18ea1e8fb2cc66a07a1995143d1a907c2b8589fb
Hash <halchemist@tutamail.com>
2024-01-27 14:17:13 +0400

    feat: Implement function which create object from user input and add to object array
    The function addBook takes user input through windows dialog box and add
    it to the array holding objects

--- Library/script.js
+++ Library/script.js
@@ -26,3 +26,14 @@ Book.prototype.hasRead = function()
 {
     return this.read;
 }
+
+function addBook() {
+    const title = window.prompt("What is the title of the book: ")
+    const author = window.prompt("Who is author of book: ");
+    const pages = window.prompt("How many number of pages: ");
+    const read = window.prompt("Have you read: ");
+    
+    myLibrary.push(new Book(title, author, pages, read));
+}
+
+addBook();
