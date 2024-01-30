391ffcd460a16f3d815f636c69ecc4eab3ce4df1
Hash <halchemist@tutamail.com>
2024-01-30 22:06:40 +0400

    feat: removed function addBook which window prompts user with input boxes to add a new book

--- Library/script.js
+++ Library/script.js
@@ -35,14 +35,6 @@ Book.prototype.hasRead = function()
 
 myLibrary.push(new Book("The Obstacle is the way", "Ryan Holiday", "200", "Not read"))
 
-function addBook() {
-    // const title = window.prompt("What is the title of the book: ")
-    // const author = window.prompt("Who is author of book: ");
-    // const pages = window.prompt("How many number of pages: ");
-    // const read = window.prompt("Have you read: ");
-    
-    // myLibrary.push(new Book(title, author, pages, read));
-}
 
 function displayBook()
 {
@@ -104,4 +96,3 @@ addButton.addEventListener("click", ()=>{
 
     displayBook();
 });
-
