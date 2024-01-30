4717155bed313e9a649833a2273c538688e71deb
Hash <halchemist@tutamail.com>
2024-01-30 21:44:33 +0400

    feat: Implement the add and close button
    The add and close button is implemented through functions attached to
    event listeners. The display books function has been modified to only
    display the latest book in the array instead of readding the whole books
    to table

--- Library/script.js
+++ Library/script.js
@@ -1,6 +1,10 @@
 let myLibrary = [];
 let openformButton = document.getElementById("openForm");
 let dialogBox = document.getElementById("dialog");
+let statusSelect = dialogBox.querySelector("select");
+let inputValues = dialogBox.querySelectorAll("input");
+let closeButton = document.getElementById("Close");
+let addButton = document.getElementById("Add");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -45,30 +49,50 @@ function addBook() {
 function displayBook()
 {
     let table = document.getElementById("tabul");
-    for(obj of myLibrary)
-    {
-        let tableRow = document.createElement("tr");
-        table.appendChild(tableRow);
-        let title = document.createElement("td");
-        title.textContent = obj.getTitle();
-        tableRow.appendChild(title);
-        
-        let author = document.createElement("td");
-        author.textContent = obj.getAuthor();
-        tableRow.appendChild(author);
 
-        let page = document.createElement("td");
-        page.textContent = obj.getPages();
-        tableRow.appendChild(page)
+    let obj = myLibrary[myLibrary.length-1];
 
-        let read = document.createElement("td");
-        read.textContent = obj.hasRead();
-        tableRow.appendChild(read);
-    }
+    let tableRow = document.createElement("tr");
+    table.appendChild(tableRow);
+    
+    let title = document.createElement("td");
+    title.textContent = obj.getTitle();
+    tableRow.appendChild(title);
+    
+    let author = document.createElement("td");
+    author.textContent = obj.getAuthor();
+    tableRow.appendChild(author);
+    
+    let page = document.createElement("td");
+    page.textContent = obj.getPages();
+    tableRow.appendChild(page)
+    
+    let read = document.createElement("td");
+    read.textContent = obj.hasRead();
+    tableRow.appendChild(read);
 }
 
 openformButton.addEventListener("click", ()=>{
     dialogBox.showModal();
+});
+
+closeButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    dialogBox.close();
+})
+
+addButton.addEventListener("click", ()=>{
+    let valuesArray = Array.from(inputValues);
+
+    let title = valuesArray[0].value;
+    let author = valuesArray[1].value;
+    let number = valuesArray[2].value;
+
+    let status = statusSelect.value;
+
+    myLibrary.push(new Book(title, author, number, status));
+
+    displayBook();
 });
 
 displayBook()
